7c3685c850645fe39271f46c6f833be0073ce9e4
Nicholas <71409379+Rhydli@users.noreply.github.com>
2023-02-17 10:43:17 -0500

    imports

--- tt_env/main.py
+++ tt_env/main.py
@@ -1,7 +1,23 @@
 import requests
+import pandas as pd
+import openpyxl
+from io import StringIO
 
+# GET request to API endpoint
 url = "https://api.marketdata.app/v1/stocks/candles/D/AAPL?from=2023-2-13&to=2023-2-14"
-
 response = requests.request("GET", url)
 
-print(response.text)
+# convert string into dataframe
+StringData = StringIO(response.text)
+df = pd.read_csv(StringData, sep =";")
+ 
+# print dataframe
+print(df)
+
+# write to existing excel file
+with pd.ExcelWriter('pandas_to_excel.xlsx') as writer:
+    df.to_excel(writer, sheet_name='sheet1123123')
+
+with pd.ExcelWriter('pandas_to_excel.xlsx') as writer:
+   # writer.book = openpyxl.load_workbook('pandas_to_excel.xlsx')
+    df.to_excel(writer, sheet_name='new_sheet1')
