dddf94f30423590dc314c1692c4c60a0a2fabacf
Nicholas <71409379+Rhydli@users.noreply.github.com>
2023-03-08 13:41:11 -0500

    empty/whitespace tickers, log_msg

--- tt_env/main.py
+++ tt_env/main.py
@@ -16,9 +16,6 @@ MONTH = 3
 DAY = 1
 DATE = f'{YEAR}-{MONTH}-{DAY}'
 
-'''pass in non constant lists for active and inactive tickers
-which are made from CONSTANT all tickers list'''
-
 class MyGui(QMainWindow):
 
     def __init__(self):
@@ -28,6 +25,7 @@ class MyGui(QMainWindow):
         self.setWindowTitle('Ticker Track')
         self.active_list.addItems(ACTIVE_TICKERS[:])
         self.inactive_list.addItems(INACTIVE_TICKERS[:])
+        self.log_msg = ''
 
         self.add_button.clicked.connect(lambda: self.add(self.ticker_line_edit.text()))
         self.delete_button.clicked.connect(lambda: self.delete(self.ticker_line_edit.text()))
@@ -36,47 +34,48 @@ class MyGui(QMainWindow):
 
     def add(self, ticker):
         '''TODO check if ticker is valid on api'''
-        if ticker != '':
+        if ticker and not ticker.isspace():
             if ticker in INACTIVE_TICKERS:
                 ACTIVE_TICKERS.append(ticker)
                 INACTIVE_TICKERS.remove(ticker)
-                self.list_refresh()
-                print(f'{ticker} moved to active tickers.')
+                self.log_msg = f'{ticker} moved to active tickers.'
+                self.ui_refresh()
             elif ticker not in ACTIVE_TICKERS:
                 ACTIVE_TICKERS.append(ticker)
-                self.list_refresh()
-                print(f'{ticker} added to active tickers.')
+                self.log_msg = f'{ticker} added to active tickers.'
+                self.ui_refresh()
             else:
-                print(f'{ticker} already in actively tracked tickers.')
+                self.log_msg = f'{ticker} already in actively tracked tickers.'
+                self.ui_refresh()
         else:
-            print('No Ticker provided.')
+            self.log_msg = 'No Ticker provided.'
+            self.ui_refresh()
 
     def delete(self, ticker):
-        '''check if ticker is in either list'''
-        if ticker != '':
+        if ticker and not ticker.isspace():
             if ticker in ACTIVE_TICKERS:
                 INACTIVE_TICKERS.append(ticker)
                 ACTIVE_TICKERS.remove(ticker)
-                self.list_refresh()
-                print(f'{ticker} moved to inactive tickers.')
+                self.log_msg = f'{ticker} moved to inactive tickers.'
+                self.ui_refresh()
             else:
-                print(f'{ticker} not found in actively tracked tickers.')
+                self.log_msg = f'{ticker} not found in actively tracked tickers.'
+                self.ui_refresh()
         else:
-            print('No Ticker provided.')
-
-
+            self.log_msg = 'No Ticker provided.'
+            self.ui_refresh()
 
     def toggle(self):
         pass
 
-    def list_refresh(self):
+    def ui_refresh(self):
         self.active_list.clear()
         self.active_list.addItems(ACTIVE_TICKERS[:])
         self.inactive_list.clear()
         self.inactive_list.addItems(INACTIVE_TICKERS[:])
+        self.console_message.setText(self.log_msg) 
 
     def run(self):
-        print('Starting')
         # GET requests to API, retuns <class 'requests.models.Response'>
         gets = []
         for t in ACTIVE_TICKERS:
@@ -100,9 +99,7 @@ class MyGui(QMainWindow):
         for c in CLOSE_PRICES:
             row += 1
             sheet.cell(row=row, column=4).value = c
-
         book.save(FILE_NAME)
-        print('Complete')
 
 def main():
     app = QApplication([])
