379c31123cfbbaf56b4784a2ebed0338b7a8e633
Nicholas <71409379+Rhydli@users.noreply.github.com>
2023-03-15 16:32:59 -0400

    config cleanup

--- tt_env/config.py
+++ tt_env/config.py
@@ -1,6 +1,5 @@
 import configparser
 import ast
-import os
 from datetime import date
 from datetime import timedelta
 from dateutil.parser import parse
@@ -12,21 +11,19 @@ def update_cfg():
     with open('tt_config.ini', 'w') as configfile:
         config.write(configfile)
 
-#file data
-SERVER_PATH = '\\\\dt-file\\Software\\Test\\'
-FILE_NAME = 'ticker_book.xlsx'
-SHEET_NAME = 'Sheet1'
-FILE_PATH = os.path.join(SERVER_PATH, FILE_NAME) 
+#store config file in memory on load for script use
+config = configparser.ConfigParser()
+config.read('tt_config.ini')
+
+#config parser stores all values as strings, convert to lists
+ACTIVE_TICKERS = ast.literal_eval(config['TICKERS']['active_tickers'])
+INACTIVE_TICKERS = ast.literal_eval(config['TICKERS']['inactive_tickers'])
+
 #variables to populate previous day on launch
 today = date.today()
 yesterday = today - timedelta(days = 1)
 YEAR = (str(yesterday.year))
 MONTH = (str(yesterday.month))
 DAY = (str(yesterday.day))
-#store config file in memory on load for script use
-config = configparser.ConfigParser()
-config.read('tt_config.ini')
-#config parser stores all values as strings, convert to lists
-ACTIVE_TICKERS = ast.literal_eval(config['TICKERS']['active_tickers'])
-INACTIVE_TICKERS = ast.literal_eval(config['TICKERS']['inactive_tickers'])
+
 
--- tt_env/test2.py
+++
@@ -1,4 +0,0 @@
-txt = '..aapl!@#$$%^&*/'
-mytable = str.maketrans('.', ' ')
-mydict = {46:None}
-print(txt.translate(mydict))
